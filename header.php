2fdc7c3210d0cab8bbf1dfd337035baf598d02b2
Simon BOUCHARD <simonbouchardpro@gmail.com>
2020-06-06 14:47:34 +0200

    Font changes 3.3

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<title><?php bloginfo('name') ?></title>
+<title>DIM Sciences du Texte et Connaissances Nouvelles</title>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="<?php the_field('description_du_site', 'options'); ?>"/>
