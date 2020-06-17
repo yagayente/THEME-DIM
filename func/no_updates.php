53ac797d79bb50920435a96ee84a012f4924685c
Simon BOUCHARD <simonbouchardpro@gmail.com>
2020-06-17 14:37:05 +0200

    updates

--- func/no_updates.php
+++ func/no_updates.php
@@ -9,12 +9,19 @@ Author: Simon Bouchard
 
 add_filter( 'auto_update_plugin', '__return_false' );
 add_filter( 'auto_update_theme', '__return_false' );
-function remove_core_updates(){
-    global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+
+function remove_core_updates () {
+     global $wp_version;
+     return(object) array(
+          'last_checked'=> time(),
+          'version_checked'=> $wp_version,
+          'updates' => array()
+     );
 }
 add_filter('pre_site_transient_update_core','remove_core_updates');
 add_filter('pre_site_transient_update_plugins','remove_core_updates');
 add_filter('pre_site_transient_update_themes','remove_core_updates');
+
 
 
 add_filter('site_transient_update_plugins', 'dd_remove_update_nag');
