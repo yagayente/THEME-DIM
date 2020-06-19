206d9aa7f62b976c927c34b737288aecd66d1a3f
Simon BOUCHARD <simonbouchardpro@gmail.com>
2020-06-19 15:58:15 +0200

    DIM option

--- func/page_option_acf.php
+++ func/page_option_acf.php
@@ -6,8 +6,26 @@ Version: 1.0
 Author: Simon Bouchard
 */
 
-if( function_exists('acf_add_options_page') ) {
-	acf_add_options_page();
+
+
+
+add_action('acf/init', 'my_acf_op_init');
+function my_acf_op_init() {
+
+    // Check function exists.
+    if( function_exists('acf_add_options_page') ) {
+
+        // Register options page.
+        $option_page = acf_add_options_page(array(
+            'page_title'    => __('DIM'),
+						'icon_url' => 'dashicons-arrow-right-alt',
+            'menu_title'    => __('DIM'),
+            'menu_slug'     => 'theme-general-settings',
+            'capability'    => 'edit_posts',
+            'redirect'      => false,
+						'position' => '2.3' // use decimal instead of integer to avoid conflict
+        ));
+    }
 }
 
 ?>
