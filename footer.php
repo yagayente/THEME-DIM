1e9452b0274fd69f0633a5f0d03ce7651734e22c
Simon BOUCHARD <simonbouchardpro@gmail.com>
2020-06-19 15:21:42 +0200

    footer link

--- footer.php
+++ footer.php
@@ -23,10 +23,10 @@
 
   <div class="logo4">
     <?php pll_e('all_right'); ?> &#169; <?php bloginfo('name'); ?> <?php echo date('Y'); ?><br>
-    <?php pll_e('credit'); ?>
     <?php if( have_rows('reseaux_sociaux', 'option') ): while ( have_rows('reseaux_sociaux', 'option') ) : the_row();?>
     <a href="<?php the_sub_field('lien_vers_le_reseau_social');?>" target="_blank"><?php the_sub_field('nom_du_reseau_social');?></a>
   <?php endwhile; else : endif; ?><br>
+    <?php pll_e('credit'); ?><br>
   </div>
 
 
