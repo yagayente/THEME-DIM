5e32bcd01c29929bcb0230430eee7671515ccfe6
Simon BOUCHARD <simonbouchardpro@gmail.com>
2020-06-22 15:47:07 +0200

    administration

--- func/hide_acf_and_css_for_user.php
+++ func/hide_acf_and_css_for_user.php
@@ -15,6 +15,7 @@
    return false;
  }
  }
+
  /**
   * Un css spécial pour l'administration
   */
@@ -46,6 +47,28 @@
 
          }
  }
+
+ /**
+  * ... hide  from wpadmin for all except me
+  */
+
+  function wpdocs_remove_menus(){ //enlever des menus
+
+    $current_user = wp_get_current_user();
+    $mon_nom_dutilisateur = 'simonbouchardadmin';
+    if(current_user_can('manage_options') AND $current_user->user_nicename == $mon_nom_dutilisateur){
+        return;
+    }
+      remove_menu_page( 'index.php' );
+      remove_menu_page( 'jetpack' );                    //Jetpack*
+      remove_menu_page( 'edit-comments.php' );          //Comments
+      remove_menu_page( 'themes.php' );                 //Appearance
+      remove_menu_page( 'plugins.php' );                //Plugins
+      remove_menu_page( 'tools.php' );                  //Tools
+      remove_menu_page( 'options-general.php' );
+      remove_menu_page( 'users.php' );
+  }
+  add_action( 'admin_menu', 'wpdocs_remove_menus' );
 
 
 ?>
--- func/traduire.php
+++ func/traduire.php
@@ -20,21 +20,7 @@ add_action('init', function() {
   pll_register_string('BASE_THEME', 'partager');
   pll_register_string('BASE_THEME', 'lire_plus');
   pll_register_string('BASE_THEME', 'lien_copie');
-  pll_register_string('BASE_THEME', 'copier_le_lien');
-  pll_register_string('BASE_THEME', 'message_was_send');
-  pll_register_string('BASE_THEME', 'message_was_send');
-  pll_register_string('BASE_THEME', 'votre_prenom');
-  pll_register_string('BASE_THEME', 'votre_nom');
-  pll_register_string('BASE_THEME', 'votre_email');
-  pll_register_string('BASE_THEME', 'votre_message');
-  pll_register_string('BASE_THEME', 'question');
-  pll_register_string('BASE_THEME', 'submit');
-  pll_register_string('BASE_THEME', 'message_derreur_prenom_vide');
-  pll_register_string('BASE_THEME', 'message_derreur_nom_vide');
-  pll_register_string('BASE_THEME', 'message_derreur_message_vide');
-  pll_register_string('BASE_THEME', 'message_derreur_mauvais_numero');
-  pll_register_string('BASE_THEME', 'message_derreur_email_vide');
-  pll_register_string('BASE_THEME', 'message_derreur_email_faux');
+
 });
 /*
 Use <?php pll_e('lire_plus'); ?>
